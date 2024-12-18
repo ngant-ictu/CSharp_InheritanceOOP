372ab1c7a6c25985d3b29edba0cabe6cb15b67cc
nga <nga@vietswiss.com>
2024-12-19 00:20:38 +1100

    CSharp inheritance

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace InheritanceOOP;
+
+public class Animal
+{
+    public string Name { get; set; }
+}
+
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine($" {Name} : Woof! Woof!");
+    }
+}
+
+public class Cat : Animal
+{
+    public void Meow()
+    {
+        Console.WriteLine($" {Name} : Meow! Meow!");
+    }
+}   
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Name = "Buddy";
+        dog.Bark();
+
+        Cat cat = new Cat();
+        cat.Name = "Whiskers";
+        cat.Meow();
+    }
+}
